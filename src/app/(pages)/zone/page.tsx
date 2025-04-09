8873a9a8ca61425be6328ba248127a3fa3b1b5ad
dazz6 <dazz6@kakao.com>
2025-04-10 07:55:27 +0900

    UI search-card component 작성

--- src/app/(pages)/zone/page.tsx
+++ src/app/(pages)/zone/page.tsx
@@ -1,3 +1,15 @@
+import SearchCard from "@/app/Component/shared/search-card";
+
 export default function Zone() {
-  return <div>zone</div>;
+  return (
+    <div>
+      <SearchCard
+        title="모각존"
+        description="모각에서.. 어쩌구.."
+        tags={["카페", "독서실"]}
+        sort="최신순"
+        section="모각존"
+      />
+    </div>
+  );
 }
---
+++ src/app/Component/shared/search-card.tsx
@@ -0,0 +1,48 @@
+import Button from "../ui/button";
+import Input from "../ui/input";
+import { AiOutlineSearch } from "react-icons/ai";
+
+interface SearchCardProps {
+  title: string;
+  description: string;
+  tags: string[];
+  sort: string;
+  section: string;
+}
+
+export default function SearchCard({
+  title,
+  description,
+  tags,
+  sort,
+  section,
+}: SearchCardProps) {
+  return (
+    <div className="flex flex-col items-center gap-2 w-full">
+      <span className="text-lg font-semibold">{title}</span>
+      <span className="text-sm text-border-dark">{description}</span>
+      <div className="mt-2 flex items-center gap-2">
+        <Input className="h-10 w-80 bg-white rounded-md" />
+        <div className="flex items-center h-10">
+          <AiOutlineSearch className="text-primary w-7 h-7" />
+        </div>
+      </div>
+      <div className="flex gap-10">
+        <div className="flex gap-2">
+          {tags.map((tag) => (
+            <div
+              key={tag}
+              className="border border-borders dark:border-border-dark rounded-md px-5 py-1"
+            >
+              #{tag}
+            </div>
+          ))}
+        </div>
+        <select className="bg-white rounded-md px-5 py-2">
+          <option>{sort}</option>
+        </select>
+      </div>
+      <Button>{section} 만들기</Button>
+    </div>
+  );
+}
