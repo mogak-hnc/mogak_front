ca780cc4dc1765a30ffbefa33a9e876a899b5ee0
dazz6 <dazz6@kakao.com>
2025-04-14 19:43:57 +0900

    UI header 로그인 및 회원정보 추가

--- src/app/Component/layout/header-navigation.tsx
+++ src/app/Component/layout/header-navigation.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 
 export default function HeaderNavigation() {
-  const userId = 1;
+  const userId = 0;
   return (
     <div className="text-sm gap-x-4 sm:gap-x-6 md:gap-x-9 lg:gap-x-18 flex">
       <Link href={`/zone`}>모각존</Link>
       <Link href={`/challenge`}>챌린지</Link>
       <Link href={`/advice`}>고민상담</Link>
       <Link href={`/profile/${userId}`}>프로필</Link>
+      {userId ? (
+        <Link href={`/profile/${userId}`}>회원정보</Link>
+      ) : (
+        <Link href={`/login`}>로그인</Link>
+      )}
     </div>
   );
 }
