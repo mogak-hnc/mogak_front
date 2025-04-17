aba2ba4dd7c067958fc6c34e76eeaae9ae3a2eca
dazz6 <dazz6@kakao.com>
2025-04-17 23:34:23 +0900

    UI 기본 레이아웃 padding 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
     <html lang="en" className="light">
       <body className="font-sans flex flex-col min-h-screen bg-background  text-text dark:bg-background-dark dark:text-text-dark transition-colors duration-100">
         <Header />
-        <div className="flex-grow p-4 px-16">{children}</div>
+        <div className="flex-grow px-4 sm:px-6 lg:px-36 py-4">{children}</div>
         <Footer />
       </body>
     </html>
