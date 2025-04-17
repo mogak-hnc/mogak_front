ac9b1ada9f75da8bd862f176464aaba905ebdd96
dazz6 <dazz6@kakao.com>
2025-04-17 22:40:25 +0900

    UI ui component 수정

--- src/app/Component/ui/button.tsx
+++ src/app/Component/ui/button.tsx
@@ -1,10 +1,23 @@
+type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement> & {
+  variant?: "primary" | "secondary" | "danger";
+};
+
 export default function Button({
   children,
+  variant = "secondary",
+  className = "",
   ...props
-}: React.ButtonHTMLAttributes<HTMLButtonElement>) {
+}: ButtonProps) {
+  const variantClasses = {
+    primary: "bg-primary dark:bg-primary-dark text-white",
+    secondary: "bg-secondary dark:bg-secondary-dark text-text",
+    danger: "bg-red-500 text-white",
+    etc: "bg-border-dark dark:bg-borders",
+  };
+
   return (
     <button
-      className="bg-secondary dark:bg-secondary-dark text-text px-2 py-1 mt-2 rounded-lg hover:opacity-90 transition"
+      className={`${variantClasses[variant]} px-2 py-1 mt-2 rounded-lg hover:opacity-90 transition ${className}`}
       {...props}
     >
       {children}
--- src/app/Component/ui/input.tsx
+++ src/app/Component/ui/input.tsx
@@ -3,7 +3,8 @@ export default function Input(
 ) {
   return (
     <input
-      className="w-full px-4 py-2 rounded-lg border border-border dark:border-border-dark bg-white dark:bg-background-dark text-text dark:text-text-dark focus:outline-none focus:ring-2 focus:ring-primary dark:focus:ring-primary-dark"
+      className="w-full px-4 py-2 rounded-lg border 
+      border-borders dark:border-border-dark bg-white dark:bg-border-dark text-text dark:text-text-dark focus:outline-none focus:ring-2 focus:ring-primary dark:focus:ring-primary-dark"
       {...props}
     />
   );
