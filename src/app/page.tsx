907a7ca983a8f8054368eba8baabeec274030c7b
dazz6 <dazz6@kakao.com>
2025-04-18 21:39:06 +0900

    UI 이미지 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,19 +11,29 @@ const mockStudySpaces: MainSubCardProps[] = [
     type: "studySpace",
     tag: "#카페",
     title: "카공해요",
-    participants: ["/user1.png", "/user2.png", "/user3.png", "/user4.png"],
+    participants: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3XilJeR7lqZAQ2rTEsm8XwZqguESjy-QoRQ&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRym6mPR9y_beR-ZMYSLxfQrFRqXkGXZkAeMA&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQh_3ZDacLygCOTvV-d1OJL8VJsJP4D-wFKaw&s",
+      "https://archive.myvibrary.com/original/1712540654041_a7befb52b5.jpeg",
+    ],
   },
   {
     type: "studySpace",
     tag: "#스터디룸",
     title: "조용한 방",
-    participants: ["/user1.png"],
+    participants: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJrc4NYGO3_gSoBuYSfw3JPWTpkYTuGOljJQ&s",
+    ],
   },
   {
     type: "studySpace",
     tag: "#도서관",
     title: "같이 집중해요",
-    participants: ["/user1.png", "/user2.png"],
+    participants: [
+      "https://archive.myvibrary.com/thumb/1720405364977_071dfd85af.jpeg?d=1080",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGzG4Ax9zxjUMh3I5W0diZaO7KWHNxPAIUrQ&s",
+    ],
   },
 ];
 
@@ -33,7 +43,12 @@ const mockChallenges: MainSubCardProps[] = [
     tag: "매일 인증",
     title: "매일 공부 인증하기",
     description: "2025. 01. 01 ~ 2025. 03. 25",
-    participants: ["/user1.png", "/user2.png", "/user3.png", "/user4.png"],
+    participants: [
+      "https://pbs.twimg.com/media/Fln7fcJakAM-W7m?format=jpg&name=4096x4096",
+      "https://pbs.twimg.com/media/GUtosR8bkAAAfaF?format=jpg&name=large",
+      "https://archive.myvibrary.com/thumb/1720405359192_6cd92d1f31.jpeg?d=1080",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhf8BpZb3z-P-9DGNw7tS8RdcTq_h0CEuuzA&s",
+    ],
     isOfficial: true,
   },
   {
@@ -41,7 +56,10 @@ const mockChallenges: MainSubCardProps[] = [
     tag: "영어 단어",
     title: "단어 100개 외우기",
     description: "2025. 04. 01 ~ 2025. 04. 30",
-    participants: ["/user1.png", "/user2.png"],
+    participants: [
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGzG4Ax9zxjUMh3I5W0diZaO7KWHNxPAIUrQ&s",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQkqjXM1b8mgbzIdJg5qVj-0ABUy7XvI1V37g&s",
+    ],
     isOfficial: false,
   },
   {
@@ -49,7 +67,11 @@ const mockChallenges: MainSubCardProps[] = [
     tag: "코테 대비",
     title: "백준 매일 1문제",
     description: "2025. 03. 20 ~ 2025. 05. 01",
-    participants: ["/user1.png", "/user2.png", "/user3.png"],
+    participants: [
+      "https://pbs.twimg.com/media/Fliv5LMaUAAd4wi?format=jpg&name=4096x4096",
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoXgQ9r3KP0Qcki63OTV-hv_A_M6LFXwWhag&s",
+      "https://mblogthumb-phinf.pstatic.net/MjAyNDA4MDhfMTE0/MDAxNzIzMDgwODcyNjcy.eJhik_0f-Qv8KE3pQUmbsFSVwrktZlKoXbDPkBOdd3Yg.IdjDUJw49i7YrhD5BtURj8d-uCmE1hfq9MiU-nleQ9Ag.JPEG/SE-EA447C4B-7780-4EC4-B63E-5BF5FDCF64FC.jpg?type=w800",
+    ],
     isOfficial: true,
   },
 ];
