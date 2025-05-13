aa3efd3a6497eec2548034aa7012835c3d959d31
dazz6 <dazz6@kakao.com>
2025-05-13 14:00:58 +0900

    FIX recoil -> zustand 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import "./globals.css";
 import Header from "./Component/layout/header";
 import Footer from "./Component/layout/footer";
-import RecoilProvider from "./recoil-provider";
 
 export const metadata: Metadata = {
   title: "모각",
@@ -21,11 +20,7 @@ export default function RootLayout({
     <html lang="en" className="light">
       <body className="font-sans flex flex-col min-h-screen bg-background text-text dark:bg-background-dark dark:text-text-dark transition-colors duration-100">
         <Header />
-        <RecoilProvider>
-          <div className="flex-grow  px-4 sm:px-6 lg:px-36 py-4">
-            {children}
-          </div>
-        </RecoilProvider>
+        <div className="flex-grow  px-4 sm:px-6 lg:px-36 py-4">{children}</div>
         <Footer />
       </body>
     </html>
