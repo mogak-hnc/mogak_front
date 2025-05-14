2fa37716dcd086748a039d8950ef5fd8ece2e58b
dazz6 <dazz6@kakao.com>
2025-05-14 14:13:56 +0900

    UI 기본 페이지 tailwind 수정

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center text-primary dark:text-primary-dark">
+    <div className="flex flex-col items-center justify-center min-h-[300px] py-24 text-center text-primary dark:text-primary-dark">
       <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-primary dark:border-primary-dark mb-4"></div>
       <p className="text-sm">로딩 중입니다... 잠시만 기다려 주세요.</p>
     </div>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,13 @@
+import Link from "next/link";
+
 export default function NotFound() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen text-center text-border-dark dark:text-borders">
+    <div className="flex flex-col items-center justify-center min-h-[300px] py-24 text-center text-border-dark dark:text-borders">
       <h1 className="text-3xl font-bold mb-4">찾을 수 없는 페이지예요</h1>
       <p className="text-sm">잘못된 경로나 삭제된 페이지일 수 있어요.</p>
+      <Link className="my-5 text-primary dark:text-primary-dark" href={`/`}>
+        메인으로
+      </Link>
     </div>
   );
 }
