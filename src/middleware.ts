a228a2337d1d7505583c4eeb92295f9e93b9272f
dazz6 <dazz6@kakao.com>
2025-05-14 20:20:30 +0900

    FIX middleware 위치 변경 및 관리자 페이지 보안 포함

---
+++ src/middleware.ts
@@ -0,0 +1,50 @@
+import { NextRequest, NextResponse } from "next/server";
+import { decodeToken } from "@/utils/decode-token.util";
+
+export function middleware(request: NextRequest) {
+  const token = request.cookies.get("jwt")?.value;
+
+  const pathname = request.nextUrl.pathname;
+
+  // 로그인
+  const protectedPaths = [
+    "/zone/create",
+    "/challenge/create",
+    "/advice",
+    "/login/info",
+    "/profile",
+  ];
+  const isProtected = protectedPaths.some((path) => pathname.startsWith(path));
+
+  if (isProtected && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // 로그인 + 관리자
+  const isAdminOnly = pathname.startsWith("/admin");
+
+  if (isAdminOnly) {
+    if (!token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    const decoded = decodeToken(token);
+
+    if (!decoded || decoded.role !== "ROLE_ADMIN") {
+      return NextResponse.redirect(new URL("/", request.url));
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/zone/create",
+    "/challenge/create",
+    "/advice",
+    "/login/info",
+    "/profile",
+    "/admin/:path*",
+  ],
+};
