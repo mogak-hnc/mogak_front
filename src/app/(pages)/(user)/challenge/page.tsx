e3eb7a85035bf000d6aba80f79a0ee6545f5d024
dazz6 <dazz6@kakao.com>
2025-05-14 11:32:22 +0900

    UI 모각챌 description 문구 변경

--- src/app/(pages)/(user)/challenge/page.tsx
+++ src/app/(pages)/(user)/challenge/page.tsx
@@ -9,7 +9,7 @@ export default async function ChallengePage() {
       <SearchCard
         type="studySpace"
         title="모각챌"
-        description="모여서 각자 챌린지"
+        description="모여서 각자, 챌린지하기!"
         tags={[{ name: "공식 챌린지" }]}
         sort="최신순"
         section="챌린지"
