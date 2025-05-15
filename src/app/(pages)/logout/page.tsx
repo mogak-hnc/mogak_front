6cd09382a1b420150763e1d0151d3d5f4217a7fe
dazz6 <dazz6@kakao.com>
2025-05-15 16:09:39 +0900

    TEST 임시 로그아웃

---
+++ src/app/(pages)/logout/page.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
+
+export default function LogoutPage() {
+  const router = useRouter();
+
+  useEffect(() => {
+    document.cookie = "jwt=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT";
+
+    localStorage.removeItem("memberId");
+
+    router.push("/");
+  }, [router]);
+
+  return <div>로그아웃 중입니다...</div>;
+}
