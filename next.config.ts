f0af06dc9d1155b8d266c89939604e40f597d4c2
dazz6 <dazz6@kakao.com>
2025-05-19 18:43:31 +0900

    CHORE 리버스 프록시 설정

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,14 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://3.39.54.119:8080/api/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
