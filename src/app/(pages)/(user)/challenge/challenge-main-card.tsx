ae72cd9a50a6d4ad427c545181037904fd926fcc
dazz6 <dazz6@kakao.com>
2025-05-19 18:16:50 +0900

    UI challenge card tailwind 수정

--- src/app/(pages)/(user)/challenge/challenge-main-card.tsx
+++ src/app/(pages)/(user)/challenge/challenge-main-card.tsx
@@ -11,16 +11,20 @@ export default function ChallengeMainCard({
 }: ChallengeMainProps) {
   return (
     <Link href={`/challenge/detail/${challengeId}`}>
-      <div className="rounded-3xl p-4 shadow-md bg-white flex flex-col justify-between min-w-[280px] max-w-[320px] h-[180px]">
+      <div className="rounded-3xl p-4 shadow-md bg-white dark:bg-border-dark flex flex-col justify-between min-w-[280px] max-w-[320px] h-[180px]">
         <div className="space-y-1">
           {isOfficial && (
-            <span className="text-xs text-primary font-semibold">
+            <span className="text-xs text-primary dark:text-primary-dark font-semibold">
               공식 챌린지
             </span>
           )}
-          <p className="text-lg font-bold text-text truncate">{title}</p>
+          <p className="text-lg font-bold text-text dark:text-text-dark truncate">
+            {title}
+          </p>
           {description && (
-            <p className="text-sm text-text truncate">{description}</p>
+            <p className="text-sm text-text dark:text-text-dark truncate">
+              {description}
+            </p>
           )}
         </div>
 
@@ -30,15 +34,19 @@ export default function ChallengeMainCard({
               <img
                 key={i}
                 src={getProfileImage(src!)}
-                className="w-6 h-6 rounded-full border-2 border-white object-cover"
+                className="w-6 h-6 rounded-full border-2 border-white dark:border-border-dark object-cover"
                 alt={`participant-${i}`}
               />
             ))}
             {participants && participants.length > 3 && (
-              <span className="text-xs ml-2">+{participants.length - 3}</span>
+              <span className="text-xs ml-2 text-text dark:text-text-dark">
+                +{participants.length - 3}
+              </span>
             )}
           </div>
-          <span className="text-yellow-500 text-sm">➡️ 참가하기</span>
+          <span className="text-yellow-500 text-sm dark:text-yellow-400">
+            ➡️ 참가하기
+          </span>
         </div>
       </div>
     </Link>
