3c0e9a164bf352330ec8c9198789ce2983acb8e1
dazz6 <dazz6@kakao.com>
2025-05-19 19:16:33 +0900

    CHORE proxy 해제

--- next.config.ts
+++ next.config.ts
@@ -4,14 +4,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  async rewrites() {
-    return [
-      {
-        source: "/api/:path*",
-        destination: "http://3.39.54.119:8080/api/:path*",
-      },
-    ];
-  },
 };
 
 export default nextConfig;
