07b2b71a9dae4d2cb1c308c7c8293a40a7b87de7
dazz6 <dazz6@kakao.com>
2025-05-23 14:43:30 +0900

    UI main 에서 더보기 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,6 +9,7 @@ import { convertDate } from "@/utils/shared/date.util";
 import ChallengeMainCard from "./(pages)/(user)/challenge/challenge-main-card";
 import ZoneMainCard from "./(pages)/(user)/zone/zone-main-card";
 import { MainZoneChallenge } from "@/lib/shared/main.api";
+import Link from "next/link";
 
 export default async function Home() {
   const data = await MainZoneChallenge();
@@ -30,10 +31,17 @@ export default async function Home() {
           bgImageDark={MainBgDark.src}
         />
         <div className="flex flex-col gap-4">
-          <MainSubTitle
-            title="핫한 모각존"
-            description="현재 가장 많은 유저들이 공부 중인 모각존을 살펴 보세요."
-          />
+          <div className="flex items-center gap-10">
+            <MainSubTitle
+              title="핫한 모각존"
+              description="현재 가장 많은 유저들이 공부 중인 모각존을 살펴 보세요."
+            />
+            <Link href="/zone">
+              <span className="text-sm text-secondary dark:text-secondary-dark">
+                더보기
+              </span>
+            </Link>
+          </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {data.mogakZoneMainResponses.map((space) => (
               <ZoneMainCard
@@ -50,10 +58,17 @@ export default async function Home() {
           </div>
         </div>
         <div className="flex flex-col gap-4">
-          <MainSubTitle
-            title="지금은 챌린지 할 시간"
-            description="현재 가장 많은 유저들이 참여 중인 챌린지를 살펴 보세요."
-          />
+          <div className="flex items-center gap-10">
+            <MainSubTitle
+              title="지금은 챌린지 할 시간"
+              description="현재 가장 많은 유저들이 참여 중인 챌린지를 살펴 보세요."
+            />
+            <Link href="/challenge">
+              <span className="text-sm text-secondary dark:text-secondary-dark">
+                더보기
+              </span>
+            </Link>
+          </div>
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
             {data.mogakChallengeResponses.map((challenge) => (
               <ChallengeMainCard
