a45ee6999e8e4040541b66ca6cae1595bcdcbe38
dazz6 <dazz6@kakao.com>
2025-06-13 16:41:08 +0900

    FEAT zone 생성 loading 추가

--- src/app/(pages)/(user)/zone/create/page.tsx
+++ src/app/(pages)/(user)/zone/create/page.tsx
@@ -6,27 +6,41 @@ import ZoneCreateForm from "./zone-create-form";
 import { useZoneCreateForm } from "./use-zone-create-form";
 import { ZoneCreateInput } from "@/types/zone.type";
 import { ZoneCreatePost } from "@/lib/client/zone.client.api";
+import Loading from "@/app/loading";
 
 export default function ZoneCreatePage() {
   const router = useRouter();
+
   const [photo, setPhoto] = useState<File | null>(null);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const form = useZoneCreateForm();
 
   const createZoneSubmit = async (data: ZoneCreateInput) => {
     try {
+      setIsLoading(true);
       const res = await ZoneCreatePost(data, photo ?? undefined);
+
       router.push(`/zone/${res.mogakZoneId}`);
     } catch (err) {
       console.error("모각존 생성 실패", err);
+    } finally {
+      setIsLoading(false);
     }
   };
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
-    <ZoneCreateForm
-      form={form}
-      onSubmit={form.handleSubmit(createZoneSubmit)}
-      photo={photo}
-      setPhoto={setPhoto}
-    />
+    <>
+      <ZoneCreateForm
+        form={form}
+        onSubmit={form.handleSubmit(createZoneSubmit)}
+        photo={photo}
+        setPhoto={setPhoto}
+      />
+    </>
   );
 }
