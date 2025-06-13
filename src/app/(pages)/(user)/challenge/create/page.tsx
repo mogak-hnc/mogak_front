0740690183a13206bb5476e38af91af6045be557
dazz6 <dazz6@kakao.com>
2025-06-13 16:43:28 +0900

    FEAT challenge zone 생성 loading 추가

--- src/app/(pages)/(user)/challenge/create/page.tsx
+++ src/app/(pages)/(user)/challenge/create/page.tsx
@@ -2,18 +2,33 @@
 
 import ChallengeCreateForm from "@/app/components/challenge-create-form";
 import { useChallengeForm } from "@/app/components/use-challenge-form";
+import Loading from "@/app/loading";
 import { ChallengeCreatePost } from "@/lib/client/challenge.client.api";
 import { ChallengeForm } from "@/types/challenge.type";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function ChallengeCreatePage() {
   const form = useChallengeForm();
   const router = useRouter();
 
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
   const handleSubmit = async (data: ChallengeForm) => {
-    const res = await ChallengeCreatePost(data);
-    router.push(`/challenge/${res.challengeId}`);
+    try {
+      setIsLoading(true);
+      const res = await ChallengeCreatePost(data);
+      router.push(`/challenge/${res.challengeId}`);
+    } catch (err) {
+      console.log("챌린지 생성 실패 : ", err);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  if (isLoading) {
+    return <Loading />;
+  }
 
   return (
     <ChallengeCreateForm
