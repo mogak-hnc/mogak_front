673394158f8368ed565e7bd87c14ad58e81f0cf6
dazz6 <dazz6@kakao.com>
2025-06-25 17:31:42 +0900

    FIX never used 제거

--- src/app/(pages)/(user)/zone/[id]/(setting)/zone-detail-member.tsx
+++ src/app/(pages)/(user)/zone/[id]/(setting)/zone-detail-member.tsx
@@ -1,6 +1,5 @@
 import { ZoneDetail } from "@/lib/client/zone.client.api";
 import ZoneMemberSetting from "./zone-member-setting";
-import { getJwtFromCookie } from "@/utils/client/auth.client.util";
 
 export default async function ZoneDetailMember({
   params,
@@ -8,7 +7,6 @@ export default async function ZoneDetailMember({
   params: Promise<{ id: string }>;
 }) {
   const { id } = await params;
-  const jwt = getJwtFromCookie();
 
   const data = await ZoneDetail(id);
 
